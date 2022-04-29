a4a1a009fd698b517ee257372a038da64609dc0c
Maria22 <m.ushakova22@gmail.com>
2022-04-29 13:21:17 +0200

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type
+    this.codeA = 'A'.charCodeAt(0);
+    this.alfabetCount = 26;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) { throw new Error("Incorrect arguments!") }
+    const result = [];
+    [message, key] = [message, key].map((str) => str.toUpperCase())
+    let i = 0
+    for (let char of message) {
+      const code = char.charCodeAt(0)
+
+      if (code >= this.codeA && code <= 90) {
+        const keyCode = (key[i++ % key.length].charCodeAt(0) - this.codeA) % this.alfabetCount;
+        const charCode = code - this.codeA + keyCode;
+
+        char = String.fromCharCode(charCode % this.alfabetCount + this.codeA);
+      }
+      result.push(char)
+    }
+    return this.type ? result.join('') : result.reverse().join('')
+  }
+  decrypt(message, key) {
+    if (!message || !key) { throw new Error("Incorrect arguments!") }
+    const result = [];
+    [message, key] = [message, key].map((str) => str.toUpperCase())
+    let i = 0
+    for (let char of message) {
+      const code = char.charCodeAt(0)
+
+      if (code >= this.codeA && code <= 90) {
+        const keyCode = (key[i++ % key.length].charCodeAt(0) - this.codeA) % this.alfabetCount;
+        const charCode = code + this.codeA - keyCode;
+
+        char = String.fromCharCode(charCode % this.alfabetCount + this.codeA);
+      }
+      result.push(char)
+    }
+    return this.type ? result.join('') : result.reverse().join('')
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+//npm run test -- test/vigenere-cipher.test.js
